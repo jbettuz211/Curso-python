99a6a2b0a53529fb76da076c877e32103072f375
José Bettuz <bettuztech@gmail.com>
2026-08-10 13:01:08 -0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -13,3 +13,4 @@ print(f'Ela tem a primeira letra de cada palavra em maiusculo?: {var.istitle()}'
 print(f'Ela só tem letras?: {var.isalpha()}')
 print(f'Ela só tem números e/ou letras?: {var.isalnum()}')
 print(f'Ela esta na tabela ASSCI?: {var.isascii()}')
+print(f'Ela é um número inteiro?: {var.isdecimal()}')
