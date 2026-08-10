3170bc8904c49f9d4be8a5fae2d650f93f354eed
José Bettuz <bettuztech@gmail.com>
2026-08-10 12:51:50 -0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -8,5 +8,6 @@ var= input('Digite algo: ')
 
 #Dissecando a variavel
 print(f'O tipo da variavel é: {type(var)}')
-print(f'Ela é composta só de letras maiusculas?: {var.isupper}')
-print(f'Ela tem a primeira letra de cada palavra em maiusculo?: {var.istitle}')
+print(f'Ela é composta só de letras maiusculas?: {var.isupper()}')
+print(f'Ela tem a primeira letra de cada palavra em maiusculo?: {var.istitle()}')
+print(f'Ela só tem letras?: {var.isalpha()}')
