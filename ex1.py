6518a5171912c40c464c7c4b40da2764c4794483
José Bettuz <bettuztech@gmail.com>
2026-08-11 09:10:34 -0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -45,21 +45,4 @@ resultado= var.isprintable()
 print(f"Ela é printavel em python?: {'Sim' if resultado== True else 'Não'}")
 
 resultado= var.isspace()
-print(f"Só ha espaço na variavel?: {'Sim' if resultado== True else 'Não'}")
-
-
-
-
-#
-'''print(f'Ela é composta só de letras maiusculas?: {var.isupper()}')
-print(f'Ela tem a primeira letra de cada palavra em maiusculo?: {var.istitle()}')
-print(f'Ela só tem letras?: {var.isalpha()}')
-print(f'Ela só tem números e/ou letras?: {var.isalnum()}')
-print(f'Ela esta na tabela ASSCI?: {var.isascii()}')
-print(f'Ela é um número inteiro?: {var.isdecimal()}')
-print(f'Ela é um número sem caractereses especiais?: {var.isdigit()}')
-print(f'Nesta variavel só tem numéros?: {var.isnumeric()}')
-print(f'Ela pode ser uma variavel?: {var.isidentifier()}')
-print(f'Só tem letras minusculas nela?: {var.islower()}')
-print(f'Ela é printavel em python?: {var.isprintable()}')
-print(f'Só ha espaço na variavel?: {var.isspace()}')'''
+print(f"Só ha espaços na variavel?: {'Sim' if resultado== True else 'Não'}")
