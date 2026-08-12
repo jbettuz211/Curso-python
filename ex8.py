d9913d8ffcd62f31c1ff0bf85864cca6e41674f3
José Bettuz <bettuztech@gmail.com>
2026-08-12 16:38:22 -0300

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,11 +1,7 @@
 #Calculando descontos
 
 #Lendo o preço original do produto
-<<<<<<< HEAD
-precoOriginal= float(input('Qual o preço original do produto?: '))
-=======
 precoOriginal= float(input('Qual o preço original do produto?: )'))
->>>>>>> 89dbcbe2b68f57a59abc862c9c3448be82274846
 
 #Calculando desconto
 porcentagem= float(precoOriginal*(5/100))
