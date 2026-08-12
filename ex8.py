89dbcbe2b68f57a59abc862c9c3448be82274846
José Bettuz <bettuztech@gmail.com>
2026-08-12 09:44:10 -0300

    Create ex8.py

---
+++ ex8.py
@@ -0,0 +1,11 @@
+#Calculando descontos
+
+#Lendo o preço original do produto
+precoOriginal= float(input('Qual o preço original do produto?: )'))
+
+#Calculando desconto
+porcentagem= float(precoOriginal*(5/100))
+desconto= float(precoOriginal-porcentagem)
+
+#Printando resultado
+print(f'O produto que custava {precoOriginal}, após desconto de 5%, resultou em {desconto:.2f}R$')
