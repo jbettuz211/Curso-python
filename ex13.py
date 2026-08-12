eeb5ec46b873870738846afee2e944b47352e75a
José Bettuz <bettuztech@gmail.com>
2026-08-12 17:18:07 -0300

    Create ex13.py

---
+++ ex13.py
@@ -0,0 +1,12 @@
+#Importando a biblioteca
+from math import hypot
+
+#Lendo os dados do triangulo para calcular hipotenusa
+catetoOposto= float(input('Qual a medida do cateto oposto em metros?: '))
+catetoAdjacente= float(input('Qual a medida do cateto adjacente em metros?: '))
+
+#Calculando a hipotenusa
+hipotenusa= hypot(catetoOposto, catetoAdjacente)
+
+#Printando o resultado
+print(f'O resultado da hipotenusa dos catetos {catetoOposto}m e {catetoAdjacente}m é: {hipotenusa:.2f}m')
