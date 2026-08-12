35d980cb490c53e41ac02505b6b6091a9eb2026d
José Bettuz <bettuztech@gmail.com>
2026-08-12 16:51:49 -0300

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,10 @@
+#Conversor de temperaturas
+
+#Lendo temperatura em celsius
+celsius= float(input('Informe a temperatura em celsius: '))
+
+#Convertendo para fahrenheit
+fahrenheit= float((celsius*1.8)+32)
+
+#Printando resultado
+print(f'{celsius} Graus celsius em fahrenheit é {fahrenheit}')
